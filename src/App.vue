bd8a7bd538d3e5115fc21c86929d1f599d803a04
Andrew Byerle <abyerle@gmail.com>
2023-07-27 00:10:12 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/Welcome.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Stories" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+    <section class="hero is-fullheight oxford-blue">
+      <div class="hero-body">
+        <h1 class="title">{{ msg }}</h1>
+      </div>
+    </section>
+
+</template>
+
+<style scoped>
+
+</style>
